48cdbf7a395249ce8ebf5238d4463967ceea54dd
Taka Yanagi <saule.faucon@gmail.com>
2024-06-01 20:27:38 +0900

    Initial commit

---
+++ lib/planet/application.ex
@@ -0,0 +1,36 @@
+defmodule Planet.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      PlanetWeb.Telemetry,
+      Planet.Repo,
+      {DNSCluster, query: Application.get_env(:planet, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Planet.PubSub},
+      # Start the Finch HTTP client for sending emails
+      {Finch, name: Planet.Finch},
+      # Start a worker by calling: Planet.Worker.start_link(arg)
+      # {Planet.Worker, arg},
+      # Start to serve requests, typically the last entry
+      PlanetWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Planet.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    PlanetWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
